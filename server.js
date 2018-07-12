46dafe0bfb45d268b4ef848ebf7543f2ff8e5c5f
Alexander Nguyen <alexandernguyen@192-168-1-5.tpgi.com.au>
2018-07-13 00:28:25 +1000

    'move if process to top'

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 app.get('/', (req, res) => {
   res.json('Hello World')
 })
@@ -89,12 +96,7 @@ app.post('/api/exercise/add', (req, res) => {
   })
 })
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-}
+
 
 app.listen(process.env.PORT || 8000, () => {
   console.log('Server is listening on port 8000!')
